1acd20c1e7c113d68f089b0f0ee35f56c23a16b8
Rhone Lachner <fernandclay@gmail.com>
2020-10-27 16:47:24 -0700

    keyword dropdown list works!

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default class App extends React.Component {
   render() {
   return (
     <>
+    
 
 <Header />
 <ImageList />
--- src/ImageList.js
+++ src/ImageList.js
@@ -8,12 +8,48 @@ import images from './data';
 import React from 'react';
 
 export default class ImageList extends Component {
+    state = {
+        filterKeyword: '',
+        horns: ''
+    }
+
+handleChange = e => {
+    this.setState({
+        filterKeyword: e.target.value
+    })
+}
     render() {
         return (
            <>
            <div>
+
+            <select onChange={this.handleChange}>
+            <option value=''>Show All</option>
+            <option value='narwhal'>narwhal</option>
+            <option value='rhino'>rhino</option>
+            <option value='unicorn'>unicorn</option>
+            <option value='unilego'>unilego</option>
+            <option value='triceratops'>triceratops</option>
+            <option value='markhor'>markhor</option>
+            <option value='mouflon'>mouflon</option>
+            <option value='addax'>addax</option>
+            <option value='chameleon'>chameleon</option>
+            <option value='lizard'>lizard</option>
+            <option value='dragon'>dragon</option>
+             </select>
+       {/* THIS IS WHERE THE FILTER BEGINS */}
       {
-       images.map(item => {
+            images.filter((item) => {
+                if (!this.state.filterKeyword  ) return true;
+
+                if (item.keyword === this.state.filterKeyword)
+                return true;
+                
+                return false
+
+            })
+
+       .map(item => {
       return <ImageItem
        title={`(`+ item.title + `)`}  
        keywords={item.keyword}
@@ -24,7 +60,7 @@ export default class ImageList extends Component {
        
        /> })
        }
-
+     
     </div>
       
             </>
