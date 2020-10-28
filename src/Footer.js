82f8dbf294e6522771fc1a7d8bf3db5cad1d0901
Rhone Lachner <fernandclay@gmail.com>
2020-10-27 17:10:17 -0700

    added content to header and footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="FooterDiv">
+                Thank you for visiting!
             </div>
         )
     }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './App.css'
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="HeaderDiv">
+                Hello, and welome to the wonderful world of horned creatures.
             </div>
         )
     }
